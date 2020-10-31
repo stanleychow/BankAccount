f7786e2c3a827ecaaf622d48ae66c74da8bbc56f
stanleychow <chowtech1022@gmail.com>
2020-10-30 21:29:38 -0400

    Cleaned and commented the code

--- BankAccount.py
+++ BankAccount.py
@@ -1,18 +1,22 @@
 class BankAccount:
+    '''This class simulates a bank account
+    '''
     def __init__(self, full_name, account_number, routing_number, balance):
         self.full_name = full_name
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = balance
 
-
     def deposit(self, amount):
+        '''Adds 'amount' of money into balance
+        '''
         self.balance += amount
         print('Amount Deposited:  $'+'{:.2f}'.format(amount))
-
     
    
     def withdraw(self, amount):
+        '''Subtracts 'amount' of money from balance
+        '''
         if amount > self.balance:
             print("Insufficient funds.")
             overdraft_fee = 10
@@ -20,25 +24,31 @@ class BankAccount:
         else:
             self.balance -= amount
             print('Amount Withdrawn:  $'+'{:.2f}'.format(amount))
-        pass
+        
    
     def get_balance(self):
+        '''Returns balance
+        '''
         print('Your balance is: $'+'{:.2f}'.format(self.balance))
         return self.balance
-        pass
+        
    
     def add_interest(self):
+        '''Calculates the interest and adds it to balance
+        '''
         interest = self.balance *  0.00083
         self.balance += interest
-        pass
+        
 
    
     def print_receipt(self):
+        '''Prints recept
+        '''
         print(self.full_name)
         print("Account No.: ****" + self.account_number[-4:])
         print("Routing No.: " + self.routing_number)
         print('Balance:  $'+'{:.2f}'.format(self.balance))
-        pass
+        
 
 
 stanleyAccount = BankAccount('Stanley Chow','83057123', 'A1B1', 5)
